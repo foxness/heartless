eb3223d8db4f262663d1b274ba167579b62f64cd
nocturnal <foxnezz@gmail.com>
2019-12-03 06:04:48 +0300

    added links

--- Heartless/Heartless/routes/files.js
+++ Heartless/Heartless/routes/files.js
@@ -1,13 +1,11 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
 
 var path = require('path');
 var fs = require('fs');
 
 const rootpath = path.join(path.dirname(__dirname), 'files');
 
-router.get('/', function (req, res)
+let middleware = (req, res, next) =>
 {
     fs.readdir(rootpath, (err, files) =>
     {
@@ -18,21 +16,34 @@ router.get('/', function (req, res)
 
         let fcontent = ""
 
-        console.log(req.baseUrl)
+        let a = req.originalUrl.split('/').slice(2)
+        a.unshift("")
+        if (a.length == 1)
+        {
+            a.unshift("")
+        }
+
+        let urlpath = a.join('/')
+
+        console.log(urlpath)
 
         files.forEach((file) =>
         {
             let fullpath = path.join(rootpath, file);
             let dir = fs.lstatSync(fullpath).isDirectory()
-            fcontent += "<li>" + file + " " + dir + "</li>"
+
+            let fc = dir ? "<li><a href=\"/files/" + file + "\">" + file + "</a></li>"
+                     : "<li>" + file + "</li>"
+
+            fcontent += fc
             
             console.log(file);
         });
           
-        let content = "<!DOCTYPE html><html><body><h1>Files</h1><p>My first paragraph.</p><ul>" + fcontent + "</ul></body></html>";
+        let content = "<!DOCTYPE html><html><body><h1>Files</h1><p>" + rootpath + "</p><ul>" + fcontent + "</ul></body></html>";
 
         res.send(content);
     });
-});
+}
 
-module.exports = router;
+module.exports = middleware;
